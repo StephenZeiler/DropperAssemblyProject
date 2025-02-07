b483b71a5dc1cc825410daf68c96d1924660b077
Stephen Zeiler <stephennnlz@gmail.com>
2025-02-06 21:25:22 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ const int dirPinM1 = 23; //DIR+ Blue
 const int enPinM1 = 24; //ENA+ Red
 #define TOTAL_STEPS 1600   // Steps per cycle
 #define MIN_SPEED 10      // Minimum step delay (fastest speed, in microseconds)
-#define MAX_SPEED 450    // Maximum step delay (slowest speed, in microseconds)
+#define MAX_SPEED 100    // Maximum step delay (slowest speed, in microseconds)
 #define ACCEL_RATE 25000   // Acceleration rate (higher = faster acceleration)
 #define CYCLE_DELAY 250000 // 1-second delay between cycles (in microseconds)
 
