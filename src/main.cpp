fbe0d2f01c243b60ffc7744d8dfd7856dc5cddb9
Stephen Zeiler <stephennnlz@gmail.com>
2025-02-06 20:52:27 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ unsigned long previousSlowStepM1 = 0;
 double m1PulsePerRevMultiplier = 0.9; //.9 for 400, .45 for 800 on driver
 #define TOTAL_STEPS 1600  // Steps per cycle
 #define MIN_SPEED 50     // Minimum delay between steps (fastest speed, in microseconds) Lower value = Faster speed (smaller delay between steps).
-#define MAX_SPEED 600    // Maximum delay between steps (slowest speed, in microseconds) Lower value = Starts at a faster initial speed.
-#define ACCEL 80000        // Acceleration rate (microseconds per step reduction) Increase from 2000 to 8000 for a much quicker ramp-up.
+#define MAX_SPEED 100    // Maximum delay between steps (slowest speed, in microseconds) Lower value = Starts at a faster initial speed.
+#define ACCEL 100000        // Acceleration rate (microseconds per step reduction) Increase from 2000 to 8000 for a much quicker ramp-up.
 #define CYCLE_DELAY 1000000 // 1-second delay between cycles (in microseconds)
 
 long m1Speed = 2000; // Initial step delay (microseconds)
