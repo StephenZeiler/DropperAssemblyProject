49e7479b34e681992f98d7548ec34daa402a1349
Stephen Zeiler <stephennnlz@gmail.com>
2025-03-24 20:14:27 -0400

    test

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -6,15 +6,15 @@
 #define dirPinM1 23
 #define enPinM1 24
  // Example pin, adjust as needed
-extern unsigned long previousM1Micros = 0;
-extern unsigned long stepInterval = 1000;
-extern bool moving = false;
-extern int currentStep = 0;
-extern unsigned long cyclePauseStart = 0;
-extern const int TOTAL_STEPS = 100;
-extern const int MAX_SPEED = 1000;
-extern const int MIN_SPEED = 100;
-extern const unsigned long CYCLE_DELAY = 1000000;  // 1 second delay between cycles
+ unsigned long previousM1Micros = 0;
+ unsigned long stepInterval = 1000;
+ bool moving = false;
+ int currentStep = 0;
+ unsigned long cyclePauseStart = 0;
+ const int TOTAL_STEPS = 100;
+ const int MAX_SPEED = 1000;
+ const int MIN_SPEED = 100;
+ const unsigned long CYCLE_DELAY = 1000000;  // 1 second delay between cycles
 
 void runMotorM1() {
   pinMode(stepPinM1, OUTPUT);
