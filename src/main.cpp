1f19b9289cc74870722a76f94744e020eed90cf1
Stephen Zeiler <stephennnlz@gmail.com>
2025-03-24 20:42:11 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h>
-#include <Stepper.h>
 
-// Motors - Currently 400 steps per rev
+// Motor pins
 const int stepPinM1 = 22; // PUL+ Green Current step
 const int dirPinM1 = 23;  // DIR+ Blue
 const int enPinM1 = 24;   // ENA+ Red
 
-#define TOTAL_STEPS 100    // Total steps to move
-#define MIN_SPEED 50       // Minimum step delay (fastest speed, microseconds)
-#define MAX_SPEED 2000     // Maximum step delay (slowest speed, microseconds)
-#define ACCEL_PHASE 0.7    // 70% of distance for acceleration
-#define DECEL_PHASE 0.3    // 30% of distance for deceleration
-#define CYCLE_DELAY 250000 // Delay between cycles (microseconds)
+// Configuration
+const int TOTAL_STEPS = 100;    // Total steps to move
+const int MIN_SPEED = 50;       // Minimum step delay (fastest speed, microseconds)
+const int MAX_SPEED = 2000;     // Maximum step delay (slowest speed, microseconds)
+const float ACCEL_PHASE = 0.7f; // 70% of distance for acceleration
+const float DECEL_PHASE = 0.3f; // 30% of distance for deceleration
+const unsigned long CYCLE_DELAY = 250000; // Delay between cycles (microseconds)
 
-unsigned long previousM1Micros = 0;
-unsigned long cyclePauseStart = 0;
-int currentStep = 0;
-bool moving = true;
-bool accelerating = true;
-int m1Step = 1;
+// Motor control variables
+static unsigned long previousM1Micros = 0;
+static unsigned long cyclePauseStart = 0;
+static int currentStep = 0;
+static bool moving = true;
+static int m1Step = 1;
 
 void setup() {
   // Motors
@@ -26,14 +26,10 @@ void setup() {
   pinMode(dirPinM1, OUTPUT);
   pinMode(enPinM1, OUTPUT);
   digitalWrite(enPinM1, LOW);
-  digitalWrite(dirPinM1, HIGH); // Set direction
+  digitalWrite(dirPinM1, HIGH); // Set initial direction
 }
 
 void loop() {
-  accelerateMotorM1();
-}
-
-void accelerateMotorM1() {
   unsigned long currentMicros = micros();
   
   if (moving) {
@@ -74,13 +70,13 @@ void accelerateMotorM1() {
         moving = false;
         cyclePauseStart = micros(); // Record the pause start time
         currentStep = 0; // Reset step counter
+        digitalWrite(dirPinM1, !digitalRead(dirPinM1)); // Reverse direction for next cycle
       }
     }
   } else {
     // Pause before restarting motion
     if (currentMicros - cyclePauseStart >= CYCLE_DELAY) {
       moving = true;
-      digitalWrite(dirPinM1, !digitalRead(dirPinM1)); // Reverse direction for next cycle
     }
   }
 }
