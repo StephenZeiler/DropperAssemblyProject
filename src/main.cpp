73efa865028139014b95737954a52509f95520a9
Stephen Zeiler <stephennnlz@gmail.com>
2025-03-24 20:26:30 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,83 @@
-#include "motor_control.h"
-#include "Functions.h"
+
 #include <Arduino.h>
 #include <Stepper.h>
-//Motors
-//Currently 400 steps per rev 
-// const int stepPinM1 = 22; //PUL+ Green Current setp
-// const int dirPinM1 = 23; //DIR+ Blue
-// const int enPinM1 = 24; //ENA+ Red
-// #define TOTAL_STEPS 100   // Steps per cycle
-// #define MIN_SPEED 10      // Minimum step delay (fastest speed, in microseconds)
-// #define MAX_SPEED 450    // Maximum step delay (slowest speed, in microseconds)
-// #define ACCEL_RATE 30000   // Acceleration rate (higher = faster acceleration)
-// #define CYCLE_DELAY 250000 // 1-second delay between cycles (in microseconds)
+// Motor pins
+const int stepPinM1 = 22; // PUL+ Green Current step
+const int dirPinM1 = 23;  // DIR+ Blue
+const int enPinM1 = 24;   // ENA+ Red
 
-// unsigned long previousM1Micros = 0;
-// unsigned long cyclePauseStart = 0;
-// long stepInterval = MAX_SPEED; // Start at slowest speed
-// int m1Step = 1;
-// int currentStep = 0;
-// bool moving = true;  // Track motor movement state
+// Constants
+#define TOTAL_STEPS 100    // Steps per cycle
+#define MIN_SPEED 10       // Minimum step delay (fastest speed, in microseconds)
+#define MAX_SPEED 450      // Maximum step delay (slowest speed, in microseconds)
+#define ACCEL_RATIO 0.7    // Percentage for acceleration phase
+#define CYCLE_DELAY 1000000 // 1-second delay between cycles (in microseconds)
 
-// long m1Speed = 2000; // Initial step delay (microseconds)
-// long minSpeed = 50; // Minimum step delay (max speed, microseconds)
-// long accel = 2000;   // Acceleration rate (microseconds per step reduction)
+// Global Variables
+unsigned long previousMicros = 0;
+unsigned long cyclePauseStart = 0;
+int currentStep = 0;
+bool moving = true;  // Motor movement state
+float stepInterval = MAX_SPEED; // Initial step interval
 
-// // long calculateDegrees(long rotaryPosition) //converts the steps the stepper has stepped to degrees //a 400 step goes 0.9 degrees per step. 200 stepper motor is 1.8 degrees per step. Currently 800!
-// // {
-// //   long result = rotaryPosition * m1PulsePerRevMultiplier; 
-// //   return result;
-// // }
+void setup() {
+    // Initialize motor pins
+    pinMode(stepPinM1, OUTPUT);
+    pinMode(dirPinM1, OUTPUT);
+    pinMode(enPinM1, OUTPUT);
+    digitalWrite(enPinM1, LOW);  // Enable motor driver
+    digitalWrite(dirPinM1, LOW); // Set direction
 
-
-// void accelerateMotorM1() {
-//    unsigned long currentMicros = micros();
-
-//   if (moving) {
-//     // Check if it's time to step
-//     if ((currentMicros - previousM1Micros) >= stepInterval) {
-//       // Make a step
-//       if (m1Step == 1) {
-//         digitalWrite(stepPinM1, HIGH);
-//         m1Step++;
-//       } else if (m1Step == 2) {
-//         digitalWrite(stepPinM1, LOW);
-//         m1Step = 1;
-//         currentStep++; // Count steps
-//       }
-
-//       previousM1Micros = currentMicros;
-
-//       // Acceleration & Deceleration using smooth curve
-//       float progress = (float)currentStep / TOTAL_STEPS;  // Progress from 0.0 to 1.0
-
-//       // Use a sine wave approach for smooth acceleration & deceleration
-//       stepInterval = MAX_SPEED - (MAX_SPEED - MIN_SPEED) * sin(progress * PI); 
-
-//       // Check if cycle is complete
-//       if (currentStep >= TOTAL_STEPS) {
-//         moving = false;
-//         cyclePauseStart = micros();  // Record the pause start time
-//         currentStep = 0; // Reset step counter
-//         stepInterval = MAX_SPEED; // Reset speed for next cycle
-//       }
-//     }
-//   } 
-//   else {
-//     // 1-second pause before restarting motion
-//     if (currentMicros - cyclePauseStart >= CYCLE_DELAY) {
-//       moving = true;
-//     }
-//   }
-// }
-
-
-// void stepM1()
-// {
-//   digitalWrite(dirPinM1, HIGH);
-//   digitalWrite(stepPinM1, HIGH);
-//   delayMicroseconds(9000);
-//   digitalWrite(stepPinM1, LOW); 
-// }
-
-void setup()
-{
- 
-  //Motors
-  // pinMode(stepPinM1, OUTPUT);
-  // pinMode(dirPinM1, OUTPUT);
-  // pinMode(enPinM1, OUTPUT);
-  // digitalWrite(enPinM1, LOW);
-  
+    Serial.begin(9600);  // Start Serial Monitor for debugging
 }
 
-void loop()
-{
-  runMotorM1();
-  //accelerateMotorM1();
-  // for (int i = 0; i < i+10; i++) {
-  //       digitalWrite(stepPinM1, HIGH);
-  //       delayMicroseconds(500);
-  //       digitalWrite(stepPinM1, LOW);
-  //       delayMicroseconds(500);
-  //   }
+void loop() {
+    if (moving) {
+        accelerateDecelerateMotor();
+    } else {
+        pauseMotor();
+    }
+}
+
+void accelerateDecelerateMotor() {
+    unsigned long currentMicros = micros();
+
+    if ((currentMicros - previousMicros) >= stepInterval) {
+        // Step the motor
+        digitalWrite(stepPinM1, HIGH);
+        delayMicroseconds(50);  // Step pulse width
+        digitalWrite(stepPinM1, LOW);
+        currentStep++;
+
+        // Calculate progress (0.0 to 1.0)
+        float progress = (float)currentStep / TOTAL_STEPS;
+
+        // Adjust speed based on progress
+        if (progress < ACCEL_RATIO) {
+            float accelProgress = pow(progress / ACCEL_RATIO, 2); // Quadratic acceleration
+            stepInterval = MAX_SPEED - (MAX_SPEED - MIN_SPEED) * accelProgress;
+        } else {
+            float decelProgress = pow((progress - ACCEL_RATIO) / (1 - ACCEL_RATIO), 2); // Quadratic deceleration
+            stepInterval = MIN_SPEED + (MAX_SPEED - MIN_SPEED) * decelProgress;
+        }
+
+        previousMicros = currentMicros;
+
+        // Stop the motor after completing all steps
+        if (currentStep >= TOTAL_STEPS) {
+            moving = false;
+            cyclePauseStart = micros();
+        }
+    }
+}
+
+void pauseMotor() {
+    unsigned long currentMicros = micros();
+
+    // Pause before restarting motion
+    if (currentMicros - cyclePauseStart >= CYCLE_DELAY) {
+        currentStep = 0;         // Reset step counter
+        stepInterval = MAX_SPEED; // Reset speed
+        moving = true;           // Resume motion
+    }
 }
