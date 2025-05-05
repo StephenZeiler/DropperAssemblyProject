64445d925d0cc69bdf338629a7e9c8d94d4a9813
Stephen Zeiler <stephennnlz@gmail.com>
2025-05-04 21:41:57 -0400

    test 52

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ bool pauseRequested = false;
 bool stopRequested = false;
 
 // Bulb system pins
-const int bulbRamHomeSensorPin = 16;
+const int bulbRamHomeSensorPin = 53;
 const int bulbPositionSensorPin = 26;
 const int bulbAirPushPin = 41;
 const int bulbSeparatorPin = 37;
