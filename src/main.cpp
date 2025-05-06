359a354ceb64b46cf6615777193e9511afa005df
Stephen Zeiler <stephennnlz@gmail.com>
2025-05-06 18:04:31 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -173,7 +173,7 @@ void handleBulbSystem() {
     static unsigned long motorStartTime = 0;
     static bool ramExtended = false;
     static bool ramRetracted = true;
-    if(machine.canBulbProcessStart()){
+    // if(machine.canBulbProcessStart()){
     
     // Track motor state transitions
     if (lastMotorState && !isMoving) {
@@ -239,7 +239,7 @@ void handleBulbSystem() {
             ramRetracted = true;
         }
     }
-    }
+   // }
 }
 void handleDropperSystem() {
     static bool lastMotorState = false;
