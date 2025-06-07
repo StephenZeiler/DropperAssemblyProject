69973517532f0903df2251b50d729f913fcaded5
Stephen Zeiler <stephennnlz@gmail.com>
2025-06-06 20:50:53 -0400

    test speed

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ int currentHomePosition = 0;
 MachineState machine;
 long prevRevolverMicros = 0;  
 int revolverStep = 1;
-long revolverSpeed = 1000;
+long revolverSpeed = 750;
 void runRevolverMotor() {
   digitalWrite(revolverDIR, LOW);
   long currentMicros = micros(); // Update time inside the check
