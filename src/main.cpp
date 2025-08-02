98139737d134caf4c2f8397f26805e721e148431
Stephen Zeiler <stephennnlz@gmail.com>
2025-08-02 14:56:33 -0400

    movementPercent >= .3

--- src/main.cpp
+++ src/main.cpp
@@ -385,7 +385,7 @@ void handleBulbSystem() {
                 //runRevolverMotor(500,30,700); faster but only for 400 steps/rev
 
             }
-            if (revolverSensor == LOW && movementPercent >= .06){
+            if (revolverSensor == LOW && movementPercent >= .3){
                 machine.setShouldRevolverMove(false); 
             }                     
         }        
