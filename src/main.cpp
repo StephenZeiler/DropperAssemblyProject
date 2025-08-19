e8aa289101061ce1fee5bb35eb36e2b3f3a48af9
Stephen Zeiler <stephennnlz@gmail.com>
2025-08-19 16:56:07 -0400

    slow down revolver - still not working

--- src/main.cpp
+++ src/main.cpp
@@ -722,8 +722,9 @@ void fillRevolver() {
       armed = false;               // don’t fire again until we leave index
     } else {
       // Keep the motor stepping continuously
-      runRevolverMotor(600, 25, 700);
+      //runRevolverMotor(600, 25, 700);
       //runRevolverMotor(1200, 25, 1400);
+      runRevolverMotor(1800, 25, 2100);
     }
 
     // Re-arm ONLY after we leave index
