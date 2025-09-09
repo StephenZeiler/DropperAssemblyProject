4ff8286f5f61050facc89c58c192347119c51c71
Stephen Zeiler <stephennnlz@gmail.com>
2025-09-09 16:56:51 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -349,7 +349,7 @@ void handleBulbSystem() {
     if (preloadFiredThisStop) {
         if (micros() - preloadPulseStart >= PRELOAD_PULSE_US) {
             digitalWrite(bulbPreLoadCylinder, LOW); // retract ASAP
-            if(digitalRead(preLoadCylinderHomeSensorPin) == HIGH){ //Is home
+            if(digitalRead(preLoadCylinderHomeSensorPin) == LOW){ //Is home
                 machine.setBulbPreLoadReady(true);
             }
         }
