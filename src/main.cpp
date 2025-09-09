316fc8b378705e3b7bd7124816e6ac9007a13aa6
Stephen Zeiler <stephennnlz@gmail.com>
2025-09-09 09:37:01 -0400

    Test system with new bulb

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ bool finsihProdRequested = false;
 
 // Bulb system pins
 const int bulbRamHomeSensorPin = 33;
-const int bulbInPreLoadPosSensorPin = 26;
+const int bulbInPreLoadPosSensorPin = 26; //HIGH = something blocking sensor
 
 
 const int bulbPreLoadCylinder = 37;
@@ -766,42 +766,42 @@ int i = 0;
 
 void loop() {
     
-    // handleButtons();
-    // handleCapInjection();
-    // setSlotIdByPosition(slots);
-    // machineTracker();
+    handleButtons();
+    handleCapInjection();
+    setSlotIdByPosition(slots);
+    machineTracker();
 
-    // startTime = millis();
-    // motorPauseTime();
-    // if(!isMoving && motorPausePercent>.90){
-    //    machine.updateMachineDisplayInfo(myNex, startTime, slots);
-    // }
+    startTime = millis();
+    motorPauseTime();
+    if(!isMoving && motorPausePercent>.90){
+       machine.updateMachineDisplayInfo(myNex, startTime, slots);
+    }
     
-    // handleBulbSystem();
-    // handlePipetSystem();  // Make sure this is uncommented
+    handleBulbSystem();
+    handlePipetSystem();  // Make sure this is uncommented
     
-    // if (machine.isStopped) return;
-    // if (machine.needsHoming) {
-    //     if(machine.needsHoming){
-    //         machine.updateStatus(myNex,"Motor Homing");
-    //         homeMachine();
-    //     }
-    //     if(!machine.needsHoming && !machine.isPaused  && !machine.isStopped){
-    //         machine.updateStatus(myNex,"In Production");
-    //     }
-    //     return;
-    // }
+    if (machine.isStopped) return;
+    if (machine.needsHoming) {
+        if(machine.needsHoming){
+            machine.updateStatus(myNex,"Motor Homing");
+            homeMachine();
+        }
+        if(!machine.needsHoming && !machine.isPaused  && !machine.isStopped){
+            machine.updateStatus(myNex,"In Production");
+        }
+        return;
+    }
     
-    // if (machine.inProduction) {
-    //     stepMotor();
-    // }
+    if (machine.inProduction) {
+        stepMotor();
+    }
 
-if(digitalRead(bulbInPreLoadPosSensorPin)== HIGH){
-    digitalWrite(bulbPreLoadCylinder, HIGH);
-    delay(1000);
-    digitalWrite(bulbPreLoadCylinder, LOW);
-    delay(1000);
-}
+// if(digitalRead(bulbInPreLoadPosSensorPin)== HIGH){
+//     digitalWrite(bulbPreLoadCylinder, HIGH);
+//     delay(1000);
+//     digitalWrite(bulbPreLoadCylinder, LOW);
+//     delay(1000);
+// }
 
 // if(digitalRead(finishProductionButtonPin)){
 // digitalWrite(capInjectPin, HIGH);
