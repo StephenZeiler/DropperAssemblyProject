4596e3e4802117c928bb5181748b549aae1e509a
Stephen Zeiler <stephennnlz@gmail.com>
2025-09-09 16:56:14 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -349,16 +349,16 @@ void handleBulbSystem() {
     if (preloadFiredThisStop) {
         if (micros() - preloadPulseStart >= PRELOAD_PULSE_US) {
             digitalWrite(bulbPreLoadCylinder, LOW); // retract ASAP
-            // if(digitalRead(preLoadCylinderHomeSensorPin) == HIGH){ //Is home
-            //     machine.setBulbPreLoadReady(true);
-            // }
-        }
-        if(preLoadCylinderHome){
-
-        }
-        else if(!preLoadCylinderHome && micros() - preloadPulseStart> 20000){ //Is home
+            if(digitalRead(preLoadCylinderHomeSensorPin) == HIGH){ //Is home
                 machine.setBulbPreLoadReady(true);
+            }
         }
+        // if(preLoadCylinderHome){
+
+        // }
+        // else if(!preLoadCylinderHome && micros() - preloadPulseStart> 20000){ //Is home
+        //         machine.setBulbPreLoadReady(true);
+        // }
     }
     // If it hasn’t fired yet this stop, keep preload "not ready"
 if (!preloadFiredThisStop && machine.canPreLoadBulbProcessStart()) {
