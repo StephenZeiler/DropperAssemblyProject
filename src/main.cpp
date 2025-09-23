10b0ee595874f9dc0368fade7f9d17572526d166
Stephen Zeiler <stephennnlz@gmail.com>
2025-09-22 20:25:54 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -871,54 +871,54 @@ void setup() {
 int i = 0;
 
 void loop() {
-    // handleLowAirPressure();
-    // updatePauseAfterFromPot(); 
-    // handleButtons();
-    // handleCapInjection();
-    // setSlotIdByPosition(slots);
-    // machineTracker();
+    handleLowAirPressure();
+    updatePauseAfterFromPot(); 
+    handleButtons();
+    handleCapInjection();
+    setSlotIdByPosition(slots);
+    machineTracker();
 
-    // startTime = millis();
-    // motorPauseTime();
-    // if(!isMoving && motorPausePercent>.90){
-    //    machine.updateMachineDisplayInfo(myNex, startTime, slots);
-    // }
+    startTime = millis();
+    motorPauseTime();
+    if(!isMoving && motorPausePercent>.90){
+       machine.updateMachineDisplayInfo(myNex, startTime, slots);
+    }
     
-    // handleBulbSystem();
-    // handlePipetSystem();  // Make sure this is uncommented
+    handleBulbSystem();
+    handlePipetSystem();  // Make sure this is uncommented
     
-    // if (machine.isStopped) return;
-    // if (machine.needsHoming) {
-    //     if(machine.needsHoming){
-    //         machine.updateStatus(myNex,"Motor Homing");
-    //         homeMachine();
-    //     }
-    //     if(!machine.needsHoming && !machine.isPaused  && !machine.isStopped){
-    //         machine.updateStatus(myNex,"In Production");
-    //     }
-    //     return;
-    // }
+    if (machine.isStopped) return;
+    if (machine.needsHoming) {
+        if(machine.needsHoming){
+            machine.updateStatus(myNex,"Motor Homing");
+            homeMachine();
+        }
+        if(!machine.needsHoming && !machine.isPaused  && !machine.isStopped){
+            machine.updateStatus(myNex,"In Production");
+        }
+        return;
+    }
     
-    // if (machine.inProduction) {
-    //     stepMotor();
-    // }
+    if (machine.inProduction) {
+        stepMotor();
+    }
 
 
-bool test = false;
-if(digitalRead(lowAirSensorPin)== HIGH){
- // delay between steps
- test = false;
-}
-else{
-    test=true;
-}
+// bool test = false;
+// if(digitalRead(lowAirSensorPin)== HIGH){
+//  // delay between steps
+//  test = false;
+// }
+// else{
+//     test=true;
+// }
 
-if(test){
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(10); // pulse width
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(4000);
-}
+// if(test){
+//         digitalWrite(stepPin, HIGH);
+//         delayMicroseconds(10); // pulse width
+//         digitalWrite(stepPin, LOW);
+//         delayMicroseconds(4000);
+// }
 
 
 // if(digitalRead(finishProductionButtonPin)){
